9e14d303fb9eeabe1f2176fff7c4303b6d905bd7
Ganesh0181 <ganeshchary4001@gmail.com>
2025-11-13 06:12:01 +0530

    revert: Revert Docker optimization attempts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './tailwind.css';
+import './tailwind.css'; // Import tailwind.css
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
